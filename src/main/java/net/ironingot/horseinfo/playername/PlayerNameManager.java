637b0c81d1ce369698a5572668b53c622f1c97ca
fubira <fubira@gmail.com>
2017-02-28 12:33:38 +0900

    Get PlayerName in subthread

--- src/main/java/net/ironingot/horseinfo/playername/PlayerNameManager.java
+++ src/main/java/net/ironingot/horseinfo/playername/PlayerNameManager.java
@@ -5,49 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.common.MinecraftForge;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.InputStream;
+import com.google.gson.Gson;
 
 public class PlayerNameManager
 {
+    private final String PROFILE_URL = "https://sessionserver.mojang.com/session/minecraft/profile/";
     private Map<UUID, String> cache = new HashMap<UUID, String>();
     private List<UUID> requestList = new ArrayList<UUID>();
 
-    private final int API_ACCESS_INTERVAL = 100;
-    private int interval = 0;
-
-    public PlayerNameManager()
+    private class PlayerNameResult
     {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @SubscribeEvent
-    public void onClientTickEvent(TickEvent.ClientTickEvent event)
-    {
-        if (requestList.isEmpty())
-            return;
-
-        if (interval > 0)
-        {
-            interval --;
-            return;
-        }
-
-        UUID uuid = requestList.get(0);
-        requestList.remove(0);
-
-        String name = PlayerNameFetcher.getPlayerNameFromMojang(uuid);
-
-        if (name != null) {
-            cache.put(uuid, name);
-        } else {
-            cache.put(uuid, "Unknown Player");
-        }
-
-        interval = API_ACCESS_INTERVAL;
+        public String id;
+        public String name;
     }
 
     public String getPlayerName(UUID uuid)
@@ -56,7 +28,38 @@ public class PlayerNameManager
             return cache.get(uuid);
 
         if (!requestList.contains(uuid))
+        {
             requestList.add(uuid);
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String suuid = uuid.toString().toLowerCase().replaceAll("-", "");
+                    String name = "Unknown Player";
+
+                    try
+                    {
+                        URL url = new URL(PROFILE_URL + suuid);
+                        URLConnection con = url.openConnection();
+                        InputStream in = con.getInputStream();
+                        byte[] buffer = new byte[con.getContentLength()];
+                        in.read(buffer);
+                        String res = new String(buffer);
+                        Gson gson = new Gson();
+                        PlayerNameResult result = gson.fromJson(res, PlayerNameResult.class);
+                        cache.put(uuid, result.name);
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                    int index = requestList.indexOf(uuid);
+                    if (index >= 0)
+                        requestList.remove(index);
+                }
+            }).start();
+        }
 
         return uuid.toString().substring(0, 13);
     }
